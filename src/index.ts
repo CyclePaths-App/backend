056128ee7080787355c22866162df6193e3d2bb5
Tanisha-Somwaru <tsomwaru@albany.edu>
2025-09-24 16:52:28 -0400

    Added skeleton for cors and express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+import express = require("express");
+import cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
