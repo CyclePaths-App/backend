772106e00882344dc3bccec40e3878d7f7be3f0e
CreatorOfCryptids <dannyanddave5000@gmail.com>
2025-09-24 18:10:58 -0400

    Updated start script

--- scripts/start_db.sh
+++ scripts/start_db.sh
@@ -1,5 +1,5 @@
 # start up an already working container.
 podman start CyclePaths_DB
 # run migrations
-sleep 1 # give the container a bit to set up.
+sleep 2 # give the container a bit to set up.
 knex migrate:latest
