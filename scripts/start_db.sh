7300d21308294d35dd9c081f2ae22cc4f28af219
CreatorOfCryptids <dannyanddave5000@gmail.com>
2025-09-24 14:07:15 -0400

    Installed dependencies.

--- scripts/start_db.sh
+++ scripts/start_db.sh
@@ -1,2 +1,3 @@
-podman run -d --name CyclePaths_db --replace -e POSTGRES_PASSWORD=123abc456efg -p 5432:5432 postgres:17.2;
+# https://github.com/PaulBratslavsky/getting-started-with-knex-and-postgress?tab=readme-ov-file
+podman run -d --name=CyclePaths_DB --replace -e POSTGRES_PASSWORD=123abc456efg -p 5432:5432 postgres:17.6-alpine3.22;
 # run migrations
