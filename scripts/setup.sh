a6095b67ee39da8534dba25b06c1bc315f885859
CreatorOfCryptids <dannyanddave5000@gmail.com>
2025-09-25 23:30:36 -0400

    Completed the setup script.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,15 +1,14 @@
-# Create a postgresql container
-podman run -d --name=CyclePaths_DB --replace -e POSTGRES_PASSWORD=123abc456efg -p 5432:5432 postgres:17.6-alpine3.22;\
+echo "Downloading container (if not downloaded already), and starting the container."
+podman run -d --name=CyclePaths_DB --replace -e POSTGRES_PASSWORD=123abc456efg -p 5432:5432 postgres:17.6-alpine3.22;
 
-# TODO: some nonsense to get it to create a database called `cyclepaths_db``
-### inside container:
-# ``` bash
-# psql -U postgres          # open up the postgres interface
-# CREATE TABLE cyclepaths_db;  # make the database
-# \q                        # exit postgres interface
-# exit;                     # exit container
-# ```
+echo "Waiting for PostgreSQL to be ready..."
+until podman exec CyclePaths_DB pg_isready -U postgres > /dev/null 2>&1; do
+  sleep 1
+done
 
+echo "Creating database inside Postgresql..."
+echo 'CREATE DATABASE cyclepaths_db;\q' | podman exec -it CyclePaths_DB psql -U postgres;
 
-# Run migration
-knex migrate:latest
+echo "Running migration..."
+knex migrate:latest > /dev/null 2>&1 # Eating the output garbage to make it prettier. If you need to debug the migrations, get rid of ``> /dev/null 2>&1`
+echo "Database setup :)"
